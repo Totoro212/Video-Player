15ad69c7f08ff375600720e44f8d4a9f2b5451e0
Totoro212 <146211359+Totoro212@users.noreply.github.com>
2023-12-06 16:31:23 +0500

    Add files via upload

---
+++ Video Player/Video Player/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Video_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.progressBar1.Location = new System.Drawing.Point(0, 497);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(838, 20);
+            this.progressBar1.TabIndex = 24;
+            this.progressBar1.Visible = false;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            this.progressBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.progressBar1_MouseDown);
+            this.progressBar1.MouseEnter += new System.EventHandler(this.progressBar1_MouseEnter);
+            this.progressBar1.MouseLeave += new System.EventHandler(this.progressBar1_MouseLeave);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(677, 90);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(162, 342);
+            this.listBox1.TabIndex = 26;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseLeave += new System.EventHandler(this.listBox1_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(1, 497);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "00:00";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Enabled = false;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(789, 497);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "00:00";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button4.Location = new System.Drawing.Point(681, 395);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 37);
+            this.button4.TabIndex = 29;
+            this.button4.Text = "del";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button4.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(839, 585);
+            this.axWindowsMediaPlayer1.TabIndex = 25;
+            this.axWindowsMediaPlayer1.MouseMoveEvent += new AxWMPLib._WMPOCXEvents_MouseMoveEventHandler(this.axWindowsMediaPlayer1_MouseMoveEvent);
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            this.axWindowsMediaPlayer1.Move += new System.EventHandler(this.axWindowsMediaPlayer1_Move);
+            // 
+            // button8
+            // 
+            this.button8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button8.Image = global::Video_Player.Properties.Resources._3706870_control_list_menu_option_playlist_108704;
+            this.button8.Location = new System.Drawing.Point(800, 0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(39, 39);
+            this.button8.TabIndex = 31;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Visible = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.Image = global::Video_Player.Properties.Resources._3706876_increase_increase_sound_music_up_volume_108700;
+            this.button7.Location = new System.Drawing.Point(679, 544);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(43, 35);
+            this.button7.TabIndex = 30;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            this.button7.MouseEnter += new System.EventHandler(this.button7_MouseEnter);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button5.BackColor = System.Drawing.SystemColors.Control;
+            this.button5.Image = global::Video_Player.Properties.Resources._3706871_control_music_next_skip_108721;
+            this.button5.Location = new System.Drawing.Point(227, 544);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 37);
+            this.button5.TabIndex = 22;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Image = global::Video_Player.Properties.Resources._3706872_control_music_play_play_music_play_sound_start_108717;
+            this.button2.Location = new System.Drawing.Point(116, 544);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 37);
+            this.button2.TabIndex = 19;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Image = global::Video_Player.Properties.Resources._3706871_control_music_next_skip_1087212;
+            this.button1.Location = new System.Drawing.Point(5, 544);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 37);
+            this.button1.TabIndex = 18;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button6.Location = new System.Drawing.Point(764, 395);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(72, 37);
+            this.button6.TabIndex = 34;
+            this.button6.Text = "open";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.MouseEnter += new System.EventHandler(this.button6_MouseEnter);
+            this.button6.MouseLeave += new System.EventHandler(this.button6_MouseLeave);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.BackColor = System.Drawing.Color.Black;
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trackBar1.Location = new System.Drawing.Point(724, 540);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(115, 45);
+            this.trackBar1.TabIndex = 35;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll_1);
+            this.trackBar1.MouseLeave += new System.EventHandler(this.trackBar1_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 585);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Video Player/Video Player/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Video_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            axWindowsMediaPlayer1.uiMode = "none";
+            axWindowsMediaPlayer1.settings.volume = 20;
+            trackBar1.Value = 20;
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+            label1.ForeColor = Color.White;
+
+        }
+        string[] Path, files;
+        int x = 0, y = 0;
+        int mx = 0, my = 0;
+        ListBox ls = new ListBox();
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofp = new OpenFileDialog();
+            if (ofp.ShowDialog() == DialogResult.OK)
+            {
+                files = ofp.FileNames;
+                Path = ofp.FileNames;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    listBox1.Items.Add(files[i].Substring(files[i].LastIndexOf('\\') + 1));
+                    ls.Items.Add(files[i]);
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex -1);
+
+        }
+
+        /*private void button3_Click(object sender, EventArgs e) {listBox1.Items.Add(files[listBox1.SelectedIndex].Substring(files[listBox1.SelectedIndex].LastIndexOf('\\') +1));}*/
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = ls.Items[listBox1.SelectedIndex].ToString();
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            timer1.Enabled = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex < listBox1.Items.Count - 1)
+                ++listBox1.SelectedIndex;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPaused)
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            else
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > 0) --listBox1.SelectedIndex;
+
+        }
+
+        private void progressBar1_MouseDown(object sender, MouseEventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.currentPosition = 
+                axWindowsMediaPlayer1.currentMedia.duration * e.X / progressBar1.Width;
+
+        }
+
+
+
+        //всплывающие окна
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Visible == false)
+            {
+                listBox1.Visible = true;
+                button6.Visible = true;
+                button4.Visible = true;
+            }
+            else
+            {
+
+                button6.Visible = false;
+                button4.Visible = false;
+                listBox1.Visible = false;
+            }
+        }
+        private void button7_Click(object sender, EventArgs e){}
+        private void listBox1_MouseLeave(object sender, EventArgs e)
+        {
+        }
+        int T=0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            T++;
+
+            if (x == mx && y == my && T >= 7)
+            {
+
+                label1.Visible = false;
+                label2.Visible = false;
+                listBox1.Visible = false;
+                button6.Visible = false;
+                button4.Visible = false;
+
+                progressBar1.Visible = false;
+                button7.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+                button5.Visible = false;
+                trackBar1.Visible = false;
+                button8.Visible = false;
+                timer2.Stop();
+            }
+
+            mx = x; my = y;
+        }
+
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+           // timer2.Enabled = false;
+
+        }
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+           // timer2.Enabled = true;
+
+        }
+        private void button6_MouseLeave(object sender, EventArgs e)
+        {
+           // timer2.Enabled = true;
+        }
+        private void button6_MouseEnter(object sender, EventArgs e)
+        {
+           // timer2.Enabled = false;
+
+        }
+
+
+        private void trackBar1_MouseLeave(object sender, EventArgs e)
+        {
+            trackBar1.Visible = false;
+
+        }
+
+        private void trackBar1_Scroll_1(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.volume = trackBar1.Value;
+
+        }
+
+        private void button7_MouseEnter(object sender, EventArgs e)
+        {
+
+            if (trackBar1.Visible == false)
+            {
+                trackBar1.Visible = true;
+            }
+            else
+            {
+                trackBar1.Visible = false;
+            }
+        }
+
+      
+
+        private void axWindowsMediaPlayer1_MouseMoveEvent(object sender, AxWMPLib._WMPOCXEvents_MouseMoveEvent e)
+        {
+            x = e.fX;
+            y = e.fY;
+
+            timer2.Enabled = true;
+            timer2.Start();
+            if(x!=mx && y!=my)
+            {
+                label1.Visible = true;
+                label2.Visible = true;
+                button1.Visible = true;
+                button2.Visible = true;
+                button5.Visible = true;
+                progressBar1.Visible = true;
+                button7.Visible = true;
+                button8.Visible = true;
+                Cursor.Show();
+
+            }
+
+
+        }
+
+
+
+        //
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                progressBar1.Maximum = (int)axWindowsMediaPlayer1.Ctlcontrols.currentItem.duration;
+                progressBar1.Value = (int)axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            }
+            try
+            {
+                label1.Text = axWindowsMediaPlayer1.Ctlcontrols.currentPositionString;
+                label2.Text = axWindowsMediaPlayer1.Ctlcontrols.currentItem.durationString.ToString();
+            }
+            catch { }
+
+        }
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void progressBar1_MouseEnter(object sender, EventArgs e)
+        {
+        }
+
+        private void progressBar1_MouseLeave(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void axWindowsMediaPlayer1_Move(object sender, EventArgs e)
+        {
+        }
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+        }
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+        }
+
+
+
+    }
+}
